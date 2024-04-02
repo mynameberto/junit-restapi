f6f6a4d9b71e6760aeb1956ad5870022241d18ec
mynameberto <pedroobertasso@gmail.com>
2024-04-01 21:42:40 -0300

    start project

---
+++ src/main/java/br/com/bertasso/junitrestapi/JunitRestapiApplication.java
@@ -0,0 +1,13 @@
+package br.com.bertasso.junitrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JunitRestapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JunitRestapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/bertasso/junitrestapi/JunitRestapiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.bertasso.junitrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JunitRestapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
