427ac2ae3582eaf45af0de8865f903bb65cd14be
mynameberto <pedroobertasso@gmail.com>
2024-04-08 20:59:52 -0300

    add user controller findById

--- src/main/java/br/com/bertasso/junitrestapi/controller/UserController.java
+++ src/main/java/br/com/bertasso/junitrestapi/controller/UserController.java
@@ -2,6 +2,8 @@ package br.com.bertasso.junitrestapi.controller;
 
 
 import br.com.bertasso.junitrestapi.domain.User;
+import br.com.bertasso.junitrestapi.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,9 +14,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/users")
 public class UserController {
 
+    @Autowired
+    private UserService userService;
+
     @GetMapping(value="/id")
     public ResponseEntity<User> findById(@PathVariable Integer id) {
 
-        return ResponseEntity.ok().body(new User(1, "Valdir", "pedro@pedro.com", "12"));
+        return ResponseEntity.ok().body(userService.findById(id));
     }
 }
