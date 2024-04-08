88a79113ebb5f66ec6616077df5542ee1eaf3f51
mynameberto <pedroobertasso@gmail.com>
2024-04-08 20:59:34 -0300

    add user service impl

---
+++ src/main/java/br/com/bertasso/junitrestapi/service/impl/UserServiceImpl.java
@@ -0,0 +1,22 @@
+package br.com.bertasso.junitrestapi.service.impl;
+
+import br.com.bertasso.junitrestapi.domain.User;
+import br.com.bertasso.junitrestapi.repository.UserRepository;
+import br.com.bertasso.junitrestapi.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public User findById(Integer id) {
+        Optional<User> obj = userRepository.findById(id);
+        return obj.orElse(null);
+    }
+}
