a25011d5f270b04d2ed75c9e5e876b4192abaa54
mynameberto <pedroobertasso@gmail.com>
2024-05-27 20:57:21 -0300

    add test to findByID method from user service

---
+++ src/test/java/br/com/bertasso/junitrestapi/service/impl/UserServiceImplTest.java
@@ -0,0 +1,73 @@
+package br.com.bertasso.junitrestapi.service.impl;
+
+import br.com.bertasso.junitrestapi.domain.User;
+import br.com.bertasso.junitrestapi.domain.dto.UserDTO;
+import br.com.bertasso.junitrestapi.repository.UserRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.*;
+import org.modelmapper.ModelMapper;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.when;
+
+@SpringBootTest
+class UserServiceImplTest {
+
+    public static final Integer ID = 1;
+    public static final String NAME = "Pedro";
+    public static final String MAIL = "pedro@pedro.com";
+    public static final String PASSWORD = "123";
+    @InjectMocks private UserServiceImpl service;
+    @Mock private UserRepository repository;
+    @Mock private ModelMapper mapper;
+
+    private User user;
+    private UserDTO userDTO;
+    private Optional<User> optionalUser;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+        startUser();
+    }
+
+    @Test
+    void whenFindByIdThenReturnUserInstance() {
+        when(repository.findById(anyInt())).thenReturn(optionalUser);
+
+        User response = service.findById(ID);
+
+        assertNotNull(response);
+        assertEquals(User.class, response.getClass());
+        assertEquals(ID, response.getId());
+    }
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void addUser() {
+    }
+
+    @Test
+    void updateUser() {
+    }
+
+    @Test
+    void deleteUser() {
+    }
+
+    private void startUser() {
+
+        user = new User(ID, NAME, MAIL, PASSWORD);
+        userDTO = new UserDTO(ID, NAME, MAIL, PASSWORD);
+        optionalUser = Optional.of(new User(ID, NAME, MAIL, PASSWORD));
+
+    }
+}
